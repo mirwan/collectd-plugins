ca5dc39d0533c28313d2391801bcfc2c4027f599
Erwan Miran <mirwan666@gmail.com>
2017-07-15 18:16:50 +0200

    works. Need collectd conf to replace dot with dash in plugin and plugin instance

--- fail2ban.sh
+++ fail2ban.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 HOSTNAME="${COLLECTD_HOSTNAME:-$(/bin/hostname -s)}"
 INTERVAL="${COLLECTD_INTERVAL:-60}"
+SUDO=/usr/bin/sudo
+FAIL2BANCLIENT=/usr/bin/fail2ban-client
 PLUGIN='fail2ban-jails'
 
-FAIL2BAN_STATUS=$(fail2ban-client status)
-
-JAIL_LIST=$(fail2ban-client status | awk '/Jail list/{print}' | sed -r -e 's/^.*Jail list:[\t ]+//' -e 's/, ?/ /g')
+JAIL_LIST=$($SUDO $FAIL2BANCLIENT status | awk '/Jail list/{print}' | sed -r -e 's/^.*Jail list:[\t ]+//' -e 's/, ?/ /g')
 for jail in  ${JAIL_LIST};
 do
-	JAIL_BANNED=$(fail2ban-client status ${jail} | grep 'Currently banned' | sed -r -e 's/^[^:]+:[ \t]*//')
+	JAIL_BANNED=$($SUDO $FAIL2BANCLIENT status ${jail} | grep 'Currently banned' | sed -r -e 's/^[^:]+:[ \t]*//')
 	formatted_jail=${jail//-/.}
 	formatted_plugin=${PLUGIN//-/.}
 	printf "PUTVAL %s/%s-%s/gauge interval=%s N:%s\n" ${HOSTNAME} ${formatted_plugin} ${formatted_jail} ${INTERVAL} ${JAIL_BANNED}
