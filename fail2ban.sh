14d7c9d79899c2b4c3a4e06bbcd1052cef616896
Erwan Miran <mirwan666@gmail.com>
2017-07-15 18:14:23 +0200

    works. Need collectd conf to replace dot with dash in plugin and plugin instance

--- fail2ban.sh
+++ fail2ban.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+HOSTNAME="${COLLECTD_HOSTNAME:-$(/bin/hostname -s)}"
+INTERVAL="${COLLECTD_INTERVAL:-60}"
+PLUGIN='fail2ban-jails'
 
 FAIL2BAN_STATUS=$(fail2ban-client status)
 
-JAIL_LIST=$(echo "${FAIL2BAN_STATUS}" | awk '/Jail list/{print}' | sed -r 's/^.*Jail list:[\t ]+//'|awk 'BEGIN{RS=","}{print}')
-echo ${JAIL_LIST}
+JAIL_LIST=$(fail2ban-client status | awk '/Jail list/{print}' | sed -r -e 's/^.*Jail list:[\t ]+//' -e 's/, ?/ /g')
+for jail in  ${JAIL_LIST};
+do
+	JAIL_BANNED=$(fail2ban-client status ${jail} | grep 'Currently banned' | sed -r -e 's/^[^:]+:[ \t]*//')
+	formatted_jail=${jail//-/.}
+	formatted_plugin=${PLUGIN//-/.}
+	printf "PUTVAL %s/%s-%s/gauge interval=%s N:%s\n" ${HOSTNAME} ${formatted_plugin} ${formatted_jail} ${INTERVAL} ${JAIL_BANNED}
+done
